821cc113990f44c03460b6d8e5cfa20423355566
Sboui431 <nidhal.sboui52@gmail.com>
2021-05-03 15:14:19 +0200

    PairProgramming : Tanks Upgraden

---
+++ Assets/Scripts/TankData.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class TankLevel
+{
+    public int cost;
+    public GameObject display;
+}
+
+public class TankData : MonoBehaviour
+{
+    public List<TankLevel> level;
+    public TankLevel currentLevel;
+
+    private void OnEnable()
+    {
+        currentLevel = level[0];
+        SetTankLevel(currentLevel);
+    }
+
+    public  TankLevel GetTankLevel()
+    {
+        return currentLevel;
+    }
+
+    public void  SetTankLevel(TankLevel tanklevel)
+    {
+        currentLevel = tanklevel;
+        int currentLevelIndex = level.IndexOf(currentLevel);
+        GameObject levelDisplay = level[currentLevelIndex].display;
+
+        for (int i = 0; i < level.Count; i++)
+        {
+            if (levelDisplay != null)
+            {
+                if (i == currentLevelIndex)
+                {
+                    // Damit aktiviere ich einen Tank
+                    level[i].display.SetActive(true);
+                }
+                else
+                {
+                    // Damit deaktiviere ich einen Tank. Er ist also nicht mehr im Spiel sichtbar und verwendbar.
+                    level[i].display.SetActive(false);
+                }
+            }
+        }
+    }
+
+    public void IncreaseLevel()
+    {
+        int currentLevelIndex = level.IndexOf(currentLevel);
+        if(currentLevelIndex < level.Count - 1)
+        {
+            currentLevel = level[currentLevelIndex + 1];
+            SetTankLevel(currentLevel);
+        }
+    }
+
+    public TankLevel GetNextLevel()
+    {
+        int currentLevelIndex = level.IndexOf(currentLevel);
+        int maxLevelIndex = level.Count - 1;
+        if(currentLevelIndex < maxLevelIndex)
+        {
+            return level[currentLevelIndex + 1];
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
---
+++ Assets/Scripts/Tankplacement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tankplacement : MonoBehaviour
+{
+    public GameObject tankPrefab;
+    private GameObject tank;
+
+    private void OnMouseDown()
+    {
+        if(CanPlaceTank())
+            tank = Instantiate(tankPrefab, transform.position, Quaternion.identity);
+        else if (CanUpgradeTank())
+        {
+            tank.GetComponent<TankData>().IncreaseLevel();
+        }
+    }
+
+    private bool CanPlaceTank()
+    {
+        if(tank == null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private bool CanUpgradeTank()
+    {
+        if(tank != null)
+        {
+            TankData data = tank.GetComponent<TankData>();
+            TankLevel nextLevel = data.GetNextLevel();
+            if(nextLevel != null)
+            {
+                return true;
+            }
+           
+        }
+        return false;
+    }
+}
