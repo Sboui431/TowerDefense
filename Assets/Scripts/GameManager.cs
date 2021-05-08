12f909c777b2b4db3202a6ee6b526db3be04b034
Sboui431 <nidhal.sboui52@gmail.com>
2021-05-08 12:19:44 +0200

    Create path

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Text coinslabel;
+    public int coins = 1000;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ShowCoins();
+    }
+
+    public void SetCoins(int value)
+    {
+        coins = value;
+    }
+
+    public int GetCoins()
+    {
+        return coins;
+    }
+
+    public void ShowCoins()
+    {
+        coinslabel.GetComponent<Text>().text = "Coins: " + coins;
+    }
+}
